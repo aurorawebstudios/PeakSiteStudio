f1f9ca0b149812ce30771fad227efcdb47a9c4d5
aurorawebstudios <aurorawebestudio@gmail.com>
2026-03-22 02:28:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+// menú móvil
 const btn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
 btn.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+// animaciones scroll
+const elements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
